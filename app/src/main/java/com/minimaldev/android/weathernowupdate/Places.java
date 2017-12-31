d738511b53b64985834e8c9da5ca22d71dd7ef53
AnujPant08 <anujpant2@gmail.com>
2017-12-31 11:37:44 +0530

    Fixed SearchWindow.

--- app/src/main/java/com/minimaldev/android/weathernowupdate/Places.java
+++ app/src/main/java/com/minimaldev/android/weathernowupdate/Places.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,17 +31,10 @@ public class Places extends AppCompatActivity {
 
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         requestWindowFeature(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-        //Intent intent = getIntent();
-        //search = (Button) findViewById(R.id.button_search);
-
-        /*ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.setHomeButtonEnabled(false); // disable the button
-            actionBar.setDisplayHomeAsUpEnabled(false); // remove the left caret
-            actionBar.setDisplayShowHomeEnabled(false); // remove the icon
-        } */
 
         setContentView(R.layout.search_window);
+
+
 
         placesAutocompleteTextView=(PlacesAutocompleteTextView)findViewById(R.id.placesauto);
 
@@ -70,13 +64,19 @@ public class Places extends AppCompatActivity {
             }
         });
 
-        getSupportActionBar().setElevation(0);
-        getSupportActionBar().setTitle("Search");
+        //getSupportActionBar().setElevation(0);
+
+
+
 
     }
 
+    public void goback(View view)
+    {
+        finish();
+    }
 
-            /*public void Click_method(View view) {
+                /*public void Click_method(View view) {
                 // TODO Auto-generated method stub
                 String loc="";
                 atvPlaces = (EditText) findViewById(R.id.edit_places);
